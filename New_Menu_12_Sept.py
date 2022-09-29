bd07038b1239d408e9c2fdf1b81f43a18a2e541a
kcujjwal <kcujjwal999@gmail.com>
2022-09-29 11:06:35 +1000

    Revert "Creating a backup"
    This reverts commit b936ae6f2569cfc34b3ccd28ce09691ac665a8a5.

--- New_Menu_12_Sept.py
+++ New_Menu_12_Sept.py
@@ -9,7 +9,7 @@ import plotly.express as px
 import base64
 from streamlit_option_menu import option_menu
 
-from Pages import About,WorldMap, Diagnostics, TimeSeries, Disaster, Compare
+from Pages import About,WorldMap, Diagnostics, TimeSeries, Disaster
 
 import copy
 from pathlib import Path
